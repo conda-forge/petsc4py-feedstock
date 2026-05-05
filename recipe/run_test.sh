01ab285455ce37c11ec8a1026251e317688db7f7
Min RK <benjaminrk@gmail.com>
2026-05-05 13:38:35 -0700

    remove debug from run_test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,13 +1,4 @@
 set -ex
 cd test
-which -a python3
-env | sort
-echo "PATH=$PATH"
-ls -la $PREFIX/bin
-echo "CONDA_PREFIX=${CONDA_PREFIX:-unset}"
-echo "PREFIX=$(echo ${PREFIX:-} | sed s@/@.@g)"
-ls -la $PREFIX/bin
-echo "PYTHON=${PYTHON:-}"
-file $CONDA_PREFIX/bin/python3
 python3 -m unittest -v
 mpiexec -n 2 python3 -m unittest -v
