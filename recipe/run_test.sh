9b24c0552d152d54caf2bed292a4be1480a929f9
Min RK <benjaminrk@gmail.com>
2026-05-05 10:59:32 -0700

    debug more

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,7 +1,13 @@
 set -ex
 cd test
 which -a python3
+env | sort
+echo "PATH=$PATH"
+ls -la $PREFIX/bin
+echo "CONDA_PREFIX=${CONDA_PREFIX:-unset}"
+echo "PREFIX=$(echo ${PREFIX:-} | sed s@/@.@g)"
+ls -la $PREFIX/bin
 echo "PYTHON=${PYTHON:-}"
-file $PREFIX/bin/python3
+file $CONDA_PREFIX/bin/python3
 python3 -m unittest -v
 mpiexec -n 2 python3 -m unittest -v
